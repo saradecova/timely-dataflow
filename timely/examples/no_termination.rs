c65613c3ab11cd903ddcbab4d360fb3ae89b42d3
decovas <sara.decova@gmail.com>
2019-12-08 11:32:37 +0100

    Added internal summaries to Operate event and added <get_itnernal_structure> method to the Operate trait

---
+++ timely/examples/no_termination.rs
@@ -0,0 +1,40 @@
+extern crate timely;
+
+use timely::dataflow::{InputHandle};
+use timely::dataflow::operators::{Concat, ConnectLoop, Filter, Input, Feedback, Map};
+use timely::logging::{TimelyEvent};
+
+fn main() {
+    // initializes and runs a timely dataflow.
+    let config = timely::Configuration::from_args(::std::env::args()).unwrap();
+    timely::execute(config, |worker| {
+        
+        worker.log_register().insert::<TimelyEvent,_>("timely", |_time, data|
+            data.iter().for_each(|x| println!("LOG1: {:?}", x.2))
+        );
+        
+        let mut input = InputHandle::new();
+
+        worker.dataflow(|scope| {
+            let stream = scope.input_from(&mut input);
+            
+            let (loop_handle, loop_stream) = scope.feedback(1);
+
+            let step =
+            stream
+                .concat(&loop_stream)
+                .map(|x| if x % 2 == 0 { x / 2 } else { x - 1 })
+                .filter(|x| x > &1);
+
+            step.connect_loop(loop_handle);
+        });
+
+        for round in 0..5 {
+
+            input.send(round);
+            input.advance_to(round);
+
+        };
+
+    }).unwrap();
+}
